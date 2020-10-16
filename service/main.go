58263649e1a3f9a8d8555d2a96c3ebd299206284
RyanShang627 <dadong.shang@outlook.com>
2020-10-15 21:20:12 -0700

    feat: Add post handler

--- service/main.go
+++ service/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 // Location location of the request
 type Location struct {
@@ -18,5 +23,21 @@ type Post struct {
 }
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("started-service")
+	// All the request with endpoint "/post" will be handled by handlerPost
+	http.HandleFunc("/post", handlerPost)
+	// Start http server
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
 }
